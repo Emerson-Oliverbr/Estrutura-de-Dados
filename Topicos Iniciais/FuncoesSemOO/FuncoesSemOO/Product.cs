512b5c6ec046779855eefe8d48054150f13926ab
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-20 22:17:44 -0300

    Funções sem OO

---
+++ Topicos Iniciais/FuncoesSemOO/FuncoesSemOO/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace FuncoesSemOO
+{
+    internal class Product
+    {
+        public string? Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Preço = R$ {Price.ToString("F2", CultureInfo.InvariantCulture)}, Quantidade = {Quantity}";
+        }
+    }
+}
---
+++ Topicos Iniciais/FuncoesSemOO/FuncoesSemOO/Program.cs
@@ -0,0 +1,34 @@
+﻿using FuncoesSemOO;
+using System;
+using System.Globalization;
+
+namespace FuncoesSemOO
+{
+    internal class Program
+    {
+        static double total(Product product)
+        {
+            return product.Price * product.Quantity;
+        }
+
+        static void updatePrice(Product product, double percentage )
+        {
+            product.Price = product.Price * (1 + percentage / 100.0);
+        }
+        static void Main(string[] args)
+        {
+            Product produto1 = new Product("Laptop", 1000.00, 5);
+            Product produto2 = new Product("Headphones", 200.00, 2);
+
+            double totalProduct1 = total(produto1);
+            double totalProduct2 = total(produto2);
+
+            Console.WriteLine($" R${totalProduct1.ToString("F2",CultureInfo.InvariantCulture)}");
+            Console.WriteLine($" R${totalProduct2.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            updatePrice(produto1, 20.0);
+
+            Console.WriteLine($" R${produto1.Price.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
