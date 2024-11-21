b76a436c83ca00c8f2e48d0a35d69e8123605ebf
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-11-20 21:44:56 -0300

    Inicinado o curso com tipos estruturados

---
+++ Topicos Iniciais/TiposEstruturados/TiposEstruturados/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TiposEstruturados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product produto1 = new Product("Laptop", 1000.00, 5);
+            Product produto2 = new Product("Headphones", 200.00, 2);
+
+            Console.WriteLine($"{produto1}");
+            Console.WriteLine($"{produto2}");
+            Console.WriteLine($"{produto1.Name}");
+            Console.WriteLine($"{produto2.Name}");
+        }
+    }
+}
